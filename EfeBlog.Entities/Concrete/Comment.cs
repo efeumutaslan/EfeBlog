af290d9d0a72440db4328b3d2dfc2ee62502aa73
Efe Umut Aslan <efeumutaslan@hotmail.com>
2021-09-13 11:14:00 +0300

    Creating Database Objects.

---
+++ EfeBlog.Entities/Concrete/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EfeBlog.Shared.Entities.Abstract;
+
+namespace EfeBlog.Entities.Concrete
+{
+    public class Comment:EntityBase, IEntity
+    {
+        public string Text { get; set; }
+        public int ArticleId { get; set; }
+        public Article Article { get; set; }
+
+    }
+}
---
+++ EfeBlog.Entities/Concrete/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EfeBlog.Shared.Entities.Abstract;
+
+namespace EfeBlog.Entities.Concrete
+{
+    public class Role:EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
