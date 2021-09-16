eaa235e1b552880a552667338a194506a9975f69
Efe Umut Aslan <efeumutaslan@hotmail.com>
2021-09-16 13:58:44 +0300

    Category Manager

--- EfeBlog.Services/Concrete/CategoryManager.cs
+++ EfeBlog.Services/Concrete/CategoryManager.cs
@@ -49,24 +49,65 @@ namespace EfeBlog.Services.Concrete
             return new DataResult<IList<Category>>(ResultStatus.Error, "Kategoriler bulunamadı", null);
         }
 
-        public Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName)
+        public async Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName)
         {
-            throw new NotImplementedException();
+            await _unitOfWork.Categories.AddAsync(new Category
+            {
+                Name = categoryAddDto.Name,
+                Description = categoryAddDto.Description,
+                Note = categoryAddDto.Note,
+                IsActive = categoryAddDto.IsActive,
+                CreatedByName = createdByName,
+                CreatedDate = DateTime.Now,
+                ModifiedByName = createdByName,
+                ModifiedDate = DateTime.Now,
+                IsDeleted = false
+            }).ContinueWith(t=>_unitOfWork.SaveAsync());
+            await _unitOfWork.SaveAsync();
+            return new Result(ResultStatus.Success, $"{categoryAddDto.Name} adlı kategori başarı ile eklenmiştir.");
         }
 
-        public Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName)
+        public async Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName)
         {
-            throw new NotImplementedException();
+            var category = await _unitOfWork.Categories.GetAsync(c => c.Id == categoryUpdateDto.Id);
+            if (category!=null)
+            {
+                category.Name = categoryUpdateDto.Name;
+                category.Description = categoryUpdateDto.Description;
+                category.Note = categoryUpdateDto.Note;
+                category.IsActive = categoryUpdateDto.IsActive;
+                category.IsDeleted = categoryUpdateDto.IsDeleted;
+                category.ModifiedByName = modifiedByName;
+                category.ModifiedDate = DateTime.Now;
+                await _unitOfWork.Categories.UpdateAsync(category).ContinueWith(t => _unitOfWork.SaveAsync());
+                return new Result(ResultStatus.Success, $"{categoryUpdateDto.Name} adlı kategori başarı ile güncellenmiştir.");
+            }
+            return new Result(ResultStatus.Error, "Kategoriler bulunamadı");
         }
 
-        public Task<IResult> Delete(int categoryId)
+        public async Task<IResult> Delete(int categoryId, string modifiedByName)
         {
-            throw new NotImplementedException();
+            var category = await _unitOfWork.Categories.GetAsync(c => c.Id == categoryId);
+            if (category!=null)
+            {
+                category.IsDeleted = true;
+                category.ModifiedByName = modifiedByName;
+                category.ModifiedDate = DateTime.Now;
+                await _unitOfWork.Categories.UpdateAsync(category).ContinueWith(t => _unitOfWork.SaveAsync());
+                return new Result(ResultStatus.Success, $"{category.Name} adlı kategori başarı ile silinmiştir.");
+            }
+            return new Result(ResultStatus.Error, "Kategoriler bulunamadı");
         }
 
-        public Task<IResult> HardDelete(int categoryId)
+        public async Task<IResult> HardDelete(int categoryId)
         {
-            throw new NotImplementedException();
+            var category = await _unitOfWork.Categories.GetAsync(c => c.Id == categoryId);
+            if (category != null)
+            {
+                await _unitOfWork.Categories.DeleteAsync(category).ContinueWith(t => _unitOfWork.SaveAsync());
+                return new Result(ResultStatus.Success, $"{category.Name} adlı kategori başarı ile veritabanından silinmiştir.");
+            }
+            return new Result(ResultStatus.Error, "Kategoriler bulunamadı");
         }
     }
 }
